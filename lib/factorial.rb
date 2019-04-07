6f2a30eff3e81733c9bca499753e090e2e6a1642
K <774162+kaseea@users.noreply.github.com>
2019-04-06 19:55:16 -0700

    whelp, its sort of done, am I suppose to try recursion?

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,6 +1,13 @@
 # Computes factorial of the input number and returns it
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n), where n is the number to be factorialed and the while loop runs n-1 times.
+# Space complexity: O(1)/constant, although we are inititing a new temp value.
 def factorial(number)
-  raise NotImplementedError
+  raise ArgumentError if number == nil
+  return 1 if number == 0
+  counter = number
+  while counter > 1
+    counter -= 1
+    number *= counter
+  end
+  return number
 end
